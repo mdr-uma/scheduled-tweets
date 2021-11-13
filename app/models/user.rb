08faa9cb27d6922524a669992c880b06b4899b6c
github username <um_2003_8@hotmail.com>
2021-11-12 19:34:51 -0500

    add validation to user model to protect from bad data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+# email:string
+#password_digest:string
+#
+#password:string virtual
+#password_confirmation:string virtual
+
 class User < ApplicationRecord
     has_secure_password
+
+    validates :email, presence: true,
+               uniqueness: true,
+               format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "must be valid email address" }
+    validates :password, presence: true
 end
