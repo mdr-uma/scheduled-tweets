f8b9597c40fe698c1c2feb10327316acda12b94d
github username <um_2003_8@hotmail.com>
2021-11-14 18:39:29 -0500

    add route for logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "main#index"
 
   get "about", to: "about#index"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
 end
