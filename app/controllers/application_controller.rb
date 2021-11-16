719f89a1920f55da7f8064b7a5c5063f3d0bba30
github username <um_2003_8@hotmail.com>
2021-11-15 19:40:03 -0500

    move method from main to application controller so that it can be share with other files as well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    def set_current_user
+        if session[:user_id]
+            Current.user = User.find_by(id: session[:user_id])
+        end
+    end
 end
