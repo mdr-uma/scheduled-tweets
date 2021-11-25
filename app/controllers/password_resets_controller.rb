678d44c6868e3ba80669620da80371a3fda9dffd
github username <um_2003_8@hotmail.com>
2021-11-24 21:01:30 -0500

    define edit method that redirect to the sign in page if token expires

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,4 +11,10 @@ class PasswordResetsController < ApplicationController
 
         redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password"
     end
+
+    def edit
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path, alert: "Your token has expired. Please try again."
+    end
 end
