1067cbd425689908c8c146898b0edf693c57a7a4
github username <um_2003_8@hotmail.com>
2021-11-24 21:03:51 -0500

    define update method that update user password.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,4 +17,19 @@ class PasswordResetsController < ApplicationController
     rescue ActiveSupport::MessageVerifier::InvalidSignature
         redirect_to sign_in_path, alert: "Your token has expired. Please try again."
     end
+    
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Your password was reset successfully. Please sign in."
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def password_params
+        params.required(:user).permit(:password, :password_confirmation)
+    end
 end
